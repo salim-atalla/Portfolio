231c77419183233fbbf2dbd6723bcb71dde46ac3
salim-atalla <salimatalla.7@gmail.com>
2022-10-21 17:02:18 +0200

    correct a bug: carousel left and right btns in script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,46 +28,6 @@ const HandleDisplayNavbarMenu = () => {
 };
 HandleDisplayNavbarMenu();
 
-// Handle the projects carousel
-const carousel = document.querySelector(".projects .carousel");
-const carouselCards = document.querySelector(".projects .carousel .cards");
-const leftArrow = document.querySelector(
-  ".projects .carousel-wrapper .left-arrow i"
-);
-const rightArrow = document.querySelector(
-  ".projects .carousel-wrapper .right-arrow i"
-);
-
-let scrollAmount = 0;
-let cardsGap = 30;
-let scrollPerClick = 100;
-// document.querySelector(".projects .carousel .cards .card").clientWidth +
-// cardsGap;
-
-function carouselScrollLeft() {
-  carousel.scrollTo({
-    top: 0,
-    left: (scrollAmount -= scrollPerClick),
-    behavior: "smooth",
-  });
-  if (scrollAmount < 0) {
-    scrollAmount = 0;
-  }
-}
-
-function carouselScrollRight() {
-  if (scrollAmount <= carousel.scrollWidth - carousel.clientWidth) {
-    carousel.scrollTo({
-      top: 0,
-      left: (scrollAmount += scrollPerClick),
-      behavior: "smooth",
-    });
-  }
-}
-
-leftArrow.addEventListener("click", carouselScrollLeft);
-rightArrow.addEventListener("click", carouselScrollRight);
-
 // Function to create and add projects to the projects carousel
 function createProjectCard(url, codeUrl, img, title, description, tags) {
   const projectsContainer = document.querySelector(
@@ -141,5 +101,50 @@ async function getProjects() {
       project.tags
     );
   });
+
+  HandleProjectsCarousel();
 }
 getProjects();
+
+// Handle the projects carousel
+function HandleProjectsCarousel() {
+  const carousel = document.querySelector(".projects .carousel");
+  const carouselCards = document.querySelector(".projects .carousel .cards");
+  const leftArrow = document.querySelector(
+    ".projects .carousel-wrapper .left-arrow i"
+  );
+  const rightArrow = document.querySelector(
+    ".projects .carousel-wrapper .right-arrow i"
+  );
+
+  let scrollAmount = 0;
+  let cardsGap = 30;
+  let scrollPerClick =
+    document
+      .querySelector(".projects .carousel .cards .card")
+      .getBoundingClientRect().width + cardsGap;
+
+  const carouselScrollLeft = () => {
+    carousel.scrollTo({
+      top: 0,
+      left: (scrollAmount -= scrollPerClick),
+      behavior: "smooth",
+    });
+    if (scrollAmount < 0) {
+      scrollAmount = 0;
+    }
+  };
+
+  const carouselScrollRight = () => {
+    if (scrollAmount <= carousel.scrollWidth - carousel.clientWidth) {
+      carousel.scrollTo({
+        top: 0,
+        left: (scrollAmount += scrollPerClick),
+        behavior: "smooth",
+      });
+    }
+  };
+
+  leftArrow.addEventListener("click", carouselScrollLeft);
+  rightArrow.addEventListener("click", carouselScrollRight);
+}
